ba245a1efe710bb404940fe8d151911c1e63c4a5
Valentina <vals91giuliani@gmail.com>
2014-07-10 11:52:24 +0200

    casellapostale

--- script/makeGiardino.js
+++ script/makeGiardino.js
@@ -23,4 +23,28 @@ function makeGiardino(scene){
         {side: THREE.DoubleSide}
       );
 
+      var loader = new THREE.OBJMTLLoader();
+      loader.addEventListener('load', function (event) {
+
+        var object = event.content;
+
+        var wing2 = object.children[0];
+        var wing1 = object.children[1];
+
+        mesh = object;
+        mesh.rotation.x = Math.PI/2;
+        mesh.scale.set(2,2,2);
+        mesh.position.set(-7,-7,0)
+        scene.add(mesh);
+      
+      });
+
+
+      loader.load(
+        '/giardino/mofx_mailbox.obj',
+        '/giardino/mofx_mailbox.mtl',
+        {side: THREE.DoubleSide}
+      );
+
+
 }
